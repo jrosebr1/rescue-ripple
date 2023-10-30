a845899b4584213a2923d68082d9815947d6f09d
Adrian Rosebrock <jrosebr1@umbc.edu>
2023-10-30 13:54:17 -0400

    env

--- rescue_ripple/settings.py
+++ rescue_ripple/settings.py
@@ -26,7 +26,7 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kyrol*0!&tydyymd0(-f4-we!w_0(qavsb-@jmebr*!yyo&+q%'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
